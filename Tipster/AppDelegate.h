ae3e438310aa8c4be36bf914dd16fdb8b20d5b5b
Ana Cismaru <anacismaru@fb.com>
2020-06-23 14:00:14 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by Ana Cismaru on 6/23/20.
+//  Copyright © 2020 anacismaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by Ana Cismaru on 6/23/20.
+//  Copyright © 2020 anacismaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
